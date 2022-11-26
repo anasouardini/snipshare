35fe132f581596eb24c1118087545238ee1f354d
segfaulty1 <segfaulty@proton.me>
2022-11-26 11:16:42 +0100

    feat: notification system is done. resolve #19.

---
+++ server/src/controller/notifications.js
@@ -0,0 +1,35 @@
+const Notifications = require('../model/notifications.js');
+
+const read = async (req, res) => {
+    const response = await Notifications.read({user: req.user.username});
+    // console.log('l9 controller/notifications: ', response);
+    if (!response || !response?.[0]) {
+        return res.status(500).json({msg: 'something bad happended while feching notifications'});
+    }
+
+    return res.json({msg: response[0]});
+};
+
+const checkUnread = async (req, res) => {
+    const response = await Notifications.checkUnread({user: req.user.username});
+    // console.log('l9 controller/notifications: ', response[0]);
+    if (!response || !response?.[0]) {
+        return res.status(500).json({msg: 'something bad happended while feching notifications'});
+    }
+
+    return res.json({msg: response[0].length});
+};
+
+const markAllRead = async (req, res) => {
+    const response = await Notifications.markAllRead({user: req.user.username});
+    // console.log('l15 controller/notifications: ', response);
+    if (!response || !response?.[0]) {
+        return res
+            .status(500)
+            .json({msg: 'something bad happended while marking notifications as read'});
+    }
+
+    return res.json({msg: 'successfully marked all notifications as read'});
+};
+
+module.exports = {checkUnread, read, markAllRead};
