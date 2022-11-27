1e0948d1a697350ef62c946259060ee11463d86f
segfaulty1 <segfaulty@proton.me>
2022-11-27 23:52:01 +0100

    initializing profile page

--- Scrapyard/src/scripts/pages/profile.jsx
+++ Scrapyard/src/scripts/pages/profile.jsx
@@ -1,13 +1,33 @@
 import React from 'react';
 import Snippets from '../components/snippets';
-//import {useOutletContext} from 'react-router';
+import {useOutletContext} from 'react-router';
 
 export default function Profile() {
-    //const {whoami} = useOutletContext();
+    const {whoami, avatar, description} = useOutletContext();
 
+  //todo: art, 
     return (
-        <div className='mt-12 w-full'>
+        <div className='mt-[7rem] w-full '>
             {/* todo: profile related info */}
+            <section className='mb-10 w-[90%] max-w-[600px] mx-auto'>
+                <div className='mb-5'>
+                    <div
+                        aria-label='avatar'
+                        className='w-[90px] h-[90px] border-primary border-2 rounded-[50%] mb-5'
+                    >
+                        <button>Edit</button>
+                        <img src={avatar}></img>
+                    </div>
+                    <p>
+                        {whoami} <button>Edit</button>
+                    </p>
+                </div>
+                <details open>
+                    <summary> Profile description</summary>
+                    <p>{description}</p>
+                    <button>Edit</button>
+                </details>
+            </section>
 
             <Snippets />
         </div>
