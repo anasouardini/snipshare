d315cf83eab98ced8c64e98d88e117b1f50533fb
segfaulty1 <segfaulty@proton.me>
2023-05-26 16:40:07 +0100

    fix - deployment: forgot to export clientAddress.

--- server/src/vars.js
+++ server/src/vars.js
@@ -1,3 +1,3 @@
 let serverAddress = ''; // the first middle ware sets this up
 let clientAddress = 'https://snipshare.anasouardini.online';
-module.exports = {serverAddress}
+module.exports = {serverAddress, clientAddress}
