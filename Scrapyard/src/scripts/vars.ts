5701a870155c27c56e8429d706f452bd6fcf31a2
segfaulty1 <segfaulty@proton.me>
2023-05-27 12:30:00 +0100

    fix - deployment: fixed incorrect server address.

--- Scrapyard/src/scripts/vars.ts
+++ Scrapyard/src/scripts/vars.ts
@@ -1,5 +1,5 @@
 // const serverAddress = 'https://127.0.0.1:2000';
-const serverAddress = 'https://ec2server.duckdns.org:2000'
+const serverAddress = 'https://snipshares.anasouardini.online:2000'
 if(document.location.host.includes('localhost:') && document.location.host.includes('127.0.0.1:')){
   serverAddress = `${document.location.host.split(':')[0]}:2000`;
 }
