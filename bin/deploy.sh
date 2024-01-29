a84b9986dc26b14e1108d1614a619a629e894ba0
anasouardini <ouardinianas@proton.me>
2024-01-29 01:13:17 +0100

    setup: removed dev-specific docker setup and moved the production docker-compoe-prod's code into the main docker-compose.

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,21 +1,19 @@
 #!/bin/bash
 
-if [[ ! $1 = 'dev' && ! $1 = 'prod' || ! $2 = 'down' && ! $2 = 'up' ]]; then
-    echo "usage: deploy.sh (dev|prod) (up|down)";
-    echo "up: start/restart";
-    echo "down: kill";
-    exit 1;
-fi
+docker-compose -f docker-compose.yaml up
 
-# nav to where docker compose file is
-cd "$(dirname $0)/.."
+# if [[ ! $1 = 'dev' && ! $1 = 'prod' || ! $2 = 'down' && ! $2 = 'up' ]]; then
+#     echo "usage: deploy.sh (dev|prod) (up|down)";
+#     echo "up: start/restart";
+#     echo "down: kill";
+#     exit 1;
+# fi
 
-# deploying
-mainCompose="docker-compose.yaml";
-envCompose="docker-compose.${1}.yaml";
-action=$2;
-# echo $mainCompose
-# echo $envCompose
-# echo $action
-# exit 0;
-docker-compose -f $mainCompose -f $envCompose $action
+# # nav to where docker compose file is
+# cd "$(dirname $0)/.."
+
+# # deploying
+# mainCompose="docker-compose.yaml";
+# envCompose="docker-compose.${1}.yaml";
+# action=$2;
+# docker-compose -f $mainCompose -f $envCompose $action
